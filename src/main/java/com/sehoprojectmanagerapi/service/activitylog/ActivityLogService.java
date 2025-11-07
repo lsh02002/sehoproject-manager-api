a8874565e483aaa71dcaefefc90c088653d17459
lsh02002 <lsh02002@hanmail.net>
2025-11-07 19:08:14 +0900

    fix activitylog issue

--- src/main/java/com/sehoprojectmanagerapi/service/activitylog/ActivityLogService.java
+++ src/main/java/com/sehoprojectmanagerapi/service/activitylog/ActivityLogService.java
@@ -6,6 +6,7 @@ import com.sehoprojectmanagerapi.repository.activity.ActivityLog;
 import com.sehoprojectmanagerapi.repository.activity.ActivityLogRepository;
 import com.sehoprojectmanagerapi.repository.project.Project;
 import com.sehoprojectmanagerapi.repository.user.User;
+import com.sehoprojectmanagerapi.service.exceptions.NotAcceptableException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,11 @@ public class ActivityLogService {
     @Transactional
     public void log(ActivityEntityType type, ActivityAction action, Long targetId, String message, User actor, Project project, Object beforeJson, Object afterJson) {
         ActivityLog log = new ActivityLog(type, action, targetId, message, actor, project, beforeJson, afterJson);
+
+        if(beforeJson == afterJson) {
+            throw new NotAcceptableException("변경된 사항이 없습니다.", null);
+        }
+
         activityLogRepository.save(log);
     }
 }
